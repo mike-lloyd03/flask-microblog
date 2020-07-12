a3c3de37b126f4dd73c1b278f2b2df022d7822bc
Mike <mike@Eoghan.local>
2020-07-11 22:07:47 -0700

    Complete ch 7

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
@@ -11,4 +14,38 @@ migrate = Migrate(flask_app, db)
 login = LoginManager(flask_app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
+if not flask_app.debug:
+    if flask_app.config['MAIL_SERVER']:
+        auth = None
+        if flask_app.config['MAIL_USERNAME'] or flask_app.config['MAIL_PASSWORD']:
+            auth = (flask_app.config['MAIL_USERNAME'], flask_app.config['MAIL_PASSWORD'])
+        secure = None
+        if flask_app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(flask_app.config['MAIL_SERVER'], flask_app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + flask_app.config['MAIL_SERVER'],
+            toaddrs=flask_app.config['ADMINS'],
+            subject='Mikeroblog Failure',
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        flask_app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/mikeroblog.log',
+                                       maxBytes=10240,
+                                       backupCount=10
+                                       )
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+    file_handler.setLevel(logging.INFO)
+    flask_app.logger.addHandler(file_handler)
+
+    flask_app.logger.setLevel(logging.INFO)
+    flask_app.logger.info('Mikeroblog startup')
